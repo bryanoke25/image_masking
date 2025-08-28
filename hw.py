3074629a4ab1acd479311fe5bd2c47e290e62763
bryanoke25 <bryan.oke25@gmail.com>
2025-08-28 18:08:45 +0100

    Add files via upload

--- hw.py
+++ hw.py
@@ -1,20 +1,51 @@
 import cv2
 import numpy as np
-import time
+import time 
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
-background = 0
+count = 0
+background  = 0
 
 for i in range(60):
-    ret, background = video.read()
-    if ret == False:
+    return_val, background = video.read()
+    if return_val == False:
         continue
-
 background = np.flip(background, axis = 1)
 
 while True:
-    status,frame = video.read()
-    if status == False:
-        
+    status,frame =  video.read()
+    if not status:
+        print("cannot acess camera")
+        break
+    count += 1
+    img= np.flip(frame, axis = 1)
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_black = np.array([40, 100, 100])
+    higher_black = np.array([255, 255, 100])
+
+    mask1 = cv2.inRange(hsv, lower_black, higher_black)
+
+    lower_black = np.array([40, 40, 155])
+    higher_black = np.array([255, 255, 180])
+    
+    mask2 = cv2.inRange(hsv, lower_black, higher_black)
+
+    mask1 = mask1 + mask2 
+
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3,3), np.uint8), iterations = 2)
+    mask1 = cv2.dilate(mask1, np.ones((3,3), np.uint8), iterations  = 1)
+
+    mask2 = cv2.bitwise_not(mask1)
+
+    res1 = cv2.bitwise_and(background, background, mask = mask1)
+    res2 = cv2.bitwise_and(img, img, mask = mask2)
+    final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
+    cv2.imshow("camera", final_output)
+    k = cv2.waitKey(10)
+    if k == 27:
+        break
+video.release()
+cv2.destroyAllWindows()
 
